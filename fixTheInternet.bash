96c1724815a0f0ae5ec73f507724ca0a894d7622
FreedomBen <FreedomBen@users.noreply.github.com>
2013-04-30 10:41:53 -0600

    Clarified echo statements and fixed typo

--- fixTheInternet.bash
+++ fixTheInternet.bash
@@ -3,13 +3,13 @@
 echo "This script will temporarily disable your ethernet connection and wireless N.  This fixes a wireless N issue that Linux has with some routers.  You will need to enter your sudo password when prompted."
 
 sudo ifconfig eth0 down
-echo "Putthing eth0 down"
+echo "Putting eth0 down"
 
 sudo modprobe -r iwlwifi
-echo "Modprobing iwlwifi"
+echo "Removing iwlwifi module"
 
 sudo modprobe iwlwifi 11n_disable=1
-echo "Disabling n"
+echo "Reinsterting iwlwifi module with n disabled"
 
 echo "Done!"
 
