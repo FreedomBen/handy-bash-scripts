c80c6fa25ef424a6777628cb8c7d4a1a8fd3eaf2
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-06-05 21:41:39 -0600

    Update diskinfo to match latest bashrc

--- diskinfo.bash
+++ diskinfo.bash
@@ -4,7 +4,11 @@
 echo -e "\n\033[1;36mPartitioning (lsblk):\033[0m"
 lsblk
 
-echo -e "\n\033[1;36mUsage (df -h):\033[0m"
+echo -e "\n\033[1;36mDisk Usage (df -h):\033[0m"
 df -h
 
+echo -e "\n\033[1;36mInode Usage (df -i):\033[0m"
+df -i
+
 echo ""
+
