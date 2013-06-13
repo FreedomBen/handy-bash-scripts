9582c31d445322b1d1cfbf39931ce777e5dbca54
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-06-12 21:19:08 -0600

    Add sleep/hibernate/shutdown/reboot scripts

---
+++ reboot.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This is intended for Gnome-like DEs
+
+sudo dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend
---
+++ shutdown.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This is intended for Gnome-like DEs
+
+dbus-send --system --print-reply  --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager  org.freedesktop.ConsoleKit.Manager.Stop
