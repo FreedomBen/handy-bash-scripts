dc3e51a1a03f235cfbecbd96e526b5bdf0257849
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-09-14 14:51:43 -0600

    Don't try to fix non-Intel wifi cards

--- fixTheInternet.bash
+++ fixTheInternet.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "This script will temporarily disable your ethernet connection and wireless N.  This fixes a wireless N issue that Linux has with some routers.  You will need to enter your sudo password when prompted."
+if [ $( lsmod | grep iwlwifi ) ]; then
+    echo "This script will temporarily disable your ethernet connection and wireless N.  This fixes a wireless N issue that Linux has with some routers when using an Intel wireless card.  You will need to enter your sudo password when prompted."
 
-sudo ifconfig eth0 down
-echo "Putting eth0 down"
+    sudo ifconfig eth0 down
+    echo "Putting eth0 down"
 
-sudo modprobe -r iwlwifi
-echo "Removing iwlwifi module"
+    sudo modprobe -r iwlwifi
+    echo "Removing iwlwifi module"
 
-sudo modprobe iwlwifi 11n_disable=1
-echo "Reinsterting iwlwifi module with n disabled"
+    sudo modprobe iwlwifi 11n_disable=1
+    echo "Reinsterting iwlwifi module with n disabled"
 
-echo "Done!"
-
+    echo "Done!"
+else
+    echo "Sorry, you aren't running the iwlwifi driver!  I can't help you!"
+fi
