6998e4d8146617939d5d23cf2f46a9ef18062ed5
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-09-30 21:52:20 -0600

    Make dir if not exists when installing

--- installChangeTxPower.bash
+++ installChangeTxPower.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ ! -d $HOME/bin ]; then
+    mkdir $HOME/bin
+fi
+
 gcc changeTxPower.c -o changeTxPower && mv changeTxPower $HOME/bin/ && sudo chown root:root $HOME/bin/changeTxPower && sudo chmod +s $HOME/bin/changeTxPower
 
