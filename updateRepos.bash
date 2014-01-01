f953bcfc888118acc511566d9ca83c2ce8a86b9d
Benjamin Porter <FreedomBen@users.noreply.github.com>
2013-12-31 21:18:32 -0700

    updateRepos add ignore list option

--- updateRepos.bash
+++ updateRepos.bash
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# Ignore repos (grep regex)
+ONE='webgoat'
+TWO='unknownrepo'
+
+onIgnoreList () 
+{
+    echo "$1" | grep "$ONE" > /dev/null && return 0
+    echo "$1" | grep "$TWO" > /dev/null && return 0
+    return 1
+}
+
 for file in $(find . -maxdepth 1 -type d)
 do
     echo -en "\033[0;34mUpdating ${file}: \033[0m"
     if [ -d ${file}/.git ]; then
-        echo -e "\033[0;34mPlease enter credentials if requested\033[0m"
-        oldDir=$(pwd)
-        cd $file
-        git pull -r && git push
-        cd $oldDir
+        if onIgnoreList "${file}"; then
+            echo -e "\033[1;33mRepo on ignore list: ${file}\033[0m"
+        else
+            echo -e "\033[0;34mPlease enter credentials if requested\033[0m"
+            oldDir=$(pwd)
+            cd $file
+            git pull -r && git push
+            cd $oldDir
+        fi
     else
         echo -e "\033[0;31mNo Git repo in this folder\033[0m"
     fi
