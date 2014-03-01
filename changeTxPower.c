3e2fb0956ec34a6bd7b6b85d89134f5d0417cd37
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-02-28 20:50:16 -0700

    Change TX Power update for Fedora

--- changeTxPower.c
+++ changeTxPower.c
@@ -21,7 +21,8 @@ size_t main( size_t argc, char **argv )
 {
     if( argc == 1 )
     {
-        system( "iwconfig wlan0" );
+        // system( "iwconfig wlan0" );
+        system( "iwconfig wlp8s0" );
         printf( "\nIf you want to set the Tx Power, rerun with 1 arg\n" );
     }
     else if( argc == 2 )
@@ -35,13 +36,15 @@ size_t main( size_t argc, char **argv )
             for( i=0; i<strSize; ++i )
                 str[i] = '\x00';
 
-            snprintf( str, strSize - 1, "iwconfig wlan0 txpower %s", argv[1] );
+            // snprintf( str, strSize - 1, "iwconfig wlan0 txpower %s", argv[1] );
+            snprintf( str, strSize - 1, "iwconfig wlp8s0 txpower %s", argv[1] );
 
             size_t userid = getuid();
             setuid( geteuid() );
             system( str );
             setuid( userid );
-            system( "iwconfig wlan0" );
+            // system( "iwconfig wlan0" );
+            system( "iwconfig wlp8s0" );
             printf( "\nSet Tx Power to: %s\n", argv[1] );
             exit( 0 );
         }
