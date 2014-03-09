1d052d4c2e45dca47e47a70b148afce1512dcb3f
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-03-09 15:33:27 -0600

    updateRepos add -r switch

--- updateRepos.bash
+++ updateRepos.bash
@@ -23,6 +23,12 @@ onIgnoreList ()
     return 1
 }
 
+if [[ $@ =~ [pP] ]]; then
+    echo "Push enabled"
+else
+    echo "Push disabled (rerun with -p|--push to enable)"
+fi
+
 for file in $(find . -maxdepth 1 -type d)
 do
     echo -en "\033[0;34mUpdating ${file}: \033[0m"
@@ -33,7 +39,11 @@ do
             echo -e "\033[0;34mPlease enter credentials if requested\033[0m"
             oldDir=$(pwd)
             cd $file
-            git pull -r && git push
+            if [[ $@ =~ [pP] ]]; then
+                git pull -r && git push
+            else
+                git pull -r
+            fi
             cd $oldDir
         fi
     else
