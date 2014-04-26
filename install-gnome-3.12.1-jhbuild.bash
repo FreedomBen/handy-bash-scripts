966c0efdc4bef2e826cf1cc65e3ad2c27c9d30aa
Benjamin Porter <Benjamin.Porter@raytheon.com>
2014-04-26 17:25:33 -0600

    Add jhbuild script

---
+++ install-gnome-3.12.1-jhbuild.bash
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+die ()
+{
+    echo "ERROR: $1"
+    exit
+}
+
+sudo yum install -y @development-tools @gnome-software-development
+sudo yum install -y dotconf-devel exiv2-devel spice-protocol spice-protocol-devel gtkspell3-devel gc-devel pkg-config python-rdflib bogofilter spamassassin libunistring-devel gpgme-devel file-devel spamassassin-devel espeak-devel bogofilter-devel ppp-devel cracklib-devel cups-devel mpfr-devel libwebp-devel wireless-tools-devel ppp mpfr ragel cracklib cups gperf libtool-ltdl-devel
+
+
+if [ -d ~/gitclone ]; then
+    cd ~/gitclone
+fi
+
+
+if [ ! -d jhbuild ]; then
+    git clone git://git.gnome.org/jhbuild
+    cd jhbuild 
+    ./autogen.sh || die "Error configuring jhbuild"
+    make || die "Error building jhbuild"
+    make install || die "Error installing jhbuild"
+fi
+
+
+NEEDS_PATH_APP=0
+if [[ $PATH =~ \.local/bin ]]; then
+    NEEDS_PATH_APP=1
+else
+    while read line; do
+        if [[ $line =~ \.local/bin ]]; then
+            NEEDS_PATH_APP=1
+        fi
+    done < ~/.bashrc
+fi
+
+if (( $NEEDS_PATH_APP )); then
+    echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+fi
+
+
+mkdir -p ~/.config
+read -r -d '' VAR << "__EOF__"
+
+# -*- mode: python -*-
+# -*- coding: utf-8 -*-
+
+moduleset = [ 'http://ftp.gnome.org/pub/GNOME/teams/releng/3.12.1/gnome-apps-3.12.1.modules',
+    'http://ftp.gnome.org/pub/GNOME/teams/releng/3.12.1/gnome-suites-core-3.12.1.modules',
+    'http://ftp.gnome.org/pub/GNOME/teams/releng/3.12.1/gnome-suites-core-deps-3.12.1.modules',
+    'http://ftp.gnome.org/pub/GNOME/teams/releng/3.12.1/gnome-sysdeps-3.12.1.modules'
+    ]
+
+# A list of the modules to build.  Defaults to the GNOME core and tested apps.
+modules = [ 'meta-gnome-core', 'meta-gnome-core-shell' ]
+
+# what directory should the source be checked out to?
+checkoutroot = '~/jhbuild/checkout'
+
+# the prefix to configure/install modules to (must have write access)
+prefix = '~/jhbuild/install'
+
+nice_build = True
+
+__EOF__
+
+echo "$VAR" > ~/.config/jhbuildrc
+
+
+jhbuild sysdeps --install || die "Error installing jhbuild sysdeps"
+jhbuild build || die "Error building jhbuild"
+
+
+mkdir -p ~/jhbuild/install/var/run || die "Error making required dir"
+sudo mkdir -p /var/run/dbus || die "Error making required dir"
+sudo mkdir -p /var/lib/dbus || die "Error making required dir"
+mkdir -p ~/jhbuild/install/var/lib/dbus || die "Error making required dir"
+
+rm -rf ~/jhbuild/install/var/run/dbus
+sudo ln -s /var/run/dbus ~/jhbuild/install/var/run/dbus || die "Error making symlink"
+rm -rf ~/jhbuild/install/var/lib/dbus/machine-id
+sudo ln -s /var/lib/dbus/machine-id ~/jhbuild/install/var/lib/dbus/machine-id || die "Error smking symlink"
+
+
+mkdir -p ~/.config
+read -r -d '' VAR << "__EOF__"
+#!/bin/sh
+
+GNOME=~/jhbuild/install
+ 
+GDK_USE_XFT=1
+XDG_DATA_DIRS=$XDG_DATA_DIRS:$GNOME/share
+XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS:$GNOME/etc/xdg
+
+jhbuild run gnome-session
+
+__EOF__
+
+sudo echo "$VAR" > /usr/bin/gnome-jhbuild-session || die "Could not write /usr/bin/gnome-jhbuild-session script"
+sudo chmod a+x /usr/bin/gnome-jhbuild-session || die "Could not make session script executable"
+
+
+mkdir -p ~/.config
+read -r -d '' VAR << "__EOF__"
+[Desktop Entry]
+Name=GNOME (JHBuild)
+Comment=This session logs you into GNOME testing session
+TryExec=/usr/bin/gnome-jhbuild-session
+Exec=/usr/bin/gnome-jhbuild-session
+Icon=
+Type=Application
+
+__EOF__
+
+sudo echo "$VAR" > /usr/share/xsessions/gnome-jhbuild.desktop || die "Could not write gnome-jhbuild.desktop"
+
+read -p "Ok, we're ready to restart GDM.  This will wipe out your current X session and give you the jhbuild gnome option.  Type yes in all caps when ready: " response
+
+if [ $response =~ YES ]; then
+    sudo systemctl restart gdm
+else
+   echo "No problem, when you're ready, run: sudo systemctl restart gdm"
+fi
+
