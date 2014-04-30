735b80dc41b0b54dc80faf19b34baa523c842e69
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-04-29 22:00:06 -0600

    add pipelight install script

---
+++ pipelight-install-f20.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum install cabextract
+sudo rpm -i http://sourceforge.net/projects/mscorefonts2/files/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
+sudo wget http://download.opensuse.org/repositories/home:/DarkPlayer:/Pipelight/Fedora_20/home:DarkPlayer:Pipelight.repo -O /etc/yum.repos.d/pipelight.repo
+sudo yum install pipelight && sudo pipelight-plugin --update
+
+sudo pipelight-plugin --enable silverlight
