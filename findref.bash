d7f1f4368e4a3e2443e481f2db6f40f7a539d07c
Benjamin Porter <FreedomBen@users.noreply.github.com>
2014-05-06 21:32:10 -0600

    Findref add Git support and some debug output
    When searching inside a git repo, use the git file list so we don't
    search files the user doesn't care about (especially the .git folder)

--- findref.bash
+++ findref.bash
@@ -1,74 +1,112 @@
 #!/bin/bash
 
+DEBUG_MODE=0
+
+# Uncomment the next line to turn on debug output
+# DEBUG_MODE=1
+
 TEMP_FILE=$HOME/.findreftempfile;
-TEMP_FILE_GIT=$HOME/.findreftempfileGit;
 ABS_PATH_COMMAND="realpath"
 IN_GIT_REPO=0
-declare -a GIT_IGNORE
+
+color_light_blue='\033[1;34m'
+color_restore='\033[0m'
+
+cur_millis_since_epoch ()
+{
+    echo $(date +%3N)
+}
+
+cur_secs_since_epoch ()
+{
+    echo $(date +%s)
+}
+
+cur_secs_millis_since_epoch ()
+{
+    echo $(date +%s%3N)
+}
+
+START_DEBUG_SECS="$(cur_secs_since_epoch)"
+
+DEBUG ()
+{
+    if (( $DEBUG_MODE )); then
+        echo -e "${color_light_blue}[*] $(( $(cur_secs_since_epoch) - $START_DEBUG_SECS)).$(cur_millis_since_epoch) - DEBUG: $1${color_restore}"
+    fi
+}
+
+print_usage () 
+{
+    echo "Usage: findref [-f|--fast (skip git ignore list)] \"what text (RegEx) to look for\" \"[starting location (root dir)]\" \"[filenames to check (must match pattern)]\"";
+}
+
+
+if [ -z "$1" ]; then
+    print_usage
+    exit;
+fi
+
 
 # Need an absolute path - prefer realpath to readlink if we have it installed
 if ! $(which realpath > /dev/null 2>&1); then
     ABS_PATH_COMMAND="readlink -f"
 fi
 
-prevDir="$(pwd)"
 
-# Determine if we are inside of a Git repo so we can consider the .gitignore file
-while true; do
-    if [ -d ".git" ]; then
-        IN_GIT_REPO=1
-        break
-    fi
+# skip this if given --fast or -f
+if [[ ! $@ =~ -f ]]; then
+    DEBUG "Checking if we're in a git repo"
+    # Determine if we are inside of a Git repo so we can consider the .gitignore file
+    prevDir="$(pwd)"
 
-    if [ "$(pwd)" = "/" ]; then
-        break;
-    fi
+    while true; do
+        if [ -d ".git" ]; then
+            IN_GIT_REPO=1
+            break
+        fi
 
-    cd ..
-done
+        if [ "$(pwd)" = "/" ]; then
+            break;
+        fi
 
-cd $prevDir
-
-
-if (( $IN_GIT_REPO )); then
-    declare -a IGNORED_FILES
-
-    for i in $(git ls-files --ignored --exclude-standard); do
-        IGNORED_FILES["${#IGNORED_FILES[@]}"]="$(eval $ABS_PATH_COMMAND $i)"
+        cd ..
     done
 
-    # if we want to add untracked files to the list, uncomment this
-    # for i in $(git ls-files --exclude-standard --others); do
-    #     IGNORED_FILES["${#IGNORED_FILES[@]}"]="$(eval $ABS_PATH_COMMAND $i)"
-    # done
+    cd $prevDir
 
-    isFileIgnored () 
-    {
-        for i in "${IGNORED_FILES[@]}"; do
-            if [ "$i" = "$1" ]; then
-                return 0
-            fi
-        done
-
-        return 1
-    }
+    if (( DEBUG_MODE )); then
+        if (( IN_GIT_REPO )); then
+            DEBUG "In Git repo"
+        else
+            DEBUG "Not in a Git repo"
+        fi
+    fi
+else
+    # need to shift away the --fast or -f
+    DEBUG "Fast mode turned on, not checking for git repo"
+    shift
 fi
 
 
 # where needs to have an absolute path to properly work with our git ignore list
-    if [ -z "$2" ] || [ ! -e "$2" ]; then
+if [ -z "$2" ] || [ ! -e "$2" ]; then
     where="$(pwd)";
 else
     # prefer realpath if installed, otherwise use readlink to get absolute path
     where="$(eval $ABS_PATH_COMMAND $2)"
 fi
 
+DEBUG "Determined to search from $where"
+
 if [ -z "$1" ]; then
-    echo "Usage: findref \"what text (RegEx) to look for\" \"[starting location (root dir)]\" \"[filenames to check (must match pattern)]\"";
-    return;
+    print_usage
+    exit;
 else
     what=$(echo "$1" | sed 's/ /\\s/g');
-fi;
+fi
+
+DEBUG "Determined to search for lines matching $what"
 
 if [ -z "$3" ]; then
     filename="";
@@ -76,21 +114,26 @@ else
     filename='-iname "$3"';
 fi;
 
+DEBUG "Determined to search filenames matching $filename"
 
-eval find "$where" -type f "$filename" > $TEMP_FILE;
-
-# if filename pattern is empty, and we're in a git repo, honor the git ignore list
-if [ -z "$filename" ]; then
-    while read line; do
-        if ! isFileIgnored "$line"; then
-            echo "$line" >> "$TEMP_FILE_GIT"
-        fi
-    done < $TEMP_FILE
-
-    mv "$TEMP_FILE_GIT" "$TEMP_FILE"
+DEBUG "Populating the TEMP_FILE $TEMP_FILE with filenames to search"
+if (( $IN_GIT_REPO )); then
+    DEBUG "In Git repo, so using git's list of files"
+    for i in $(git ls-files --exclude-standard); do
+        [ -f "$i" ] && echo "$i" >> $TEMP_FILE
+    done 
+    for i in $(git ls-files --others --exclude-standard); do
+        [ -f "$i" ] && echo "$i" >> $TEMP_FILE
+    done 
+else
+    DEBUG "Not in git repo, finding all filenames matching pattern $filename"
+    eval find "$where" -type f "$filename" > $TEMP_FILE;
 fi
 
+DEBUG "Done populating the TEMP_FILE"
+
 numlines=$(cat $TEMP_FILE | wc -l);
+DEBUG "Grepping $numlines files"
 
 for ((i=1; ((1)); i+=1000 ))
 do
@@ -99,19 +142,21 @@ do
     else
         topBoundary=numlines;
     fi;
-    sed -n $i,$(( i + topBoundary ))p $TEMP_FILE | sed 's/ /\\ /g' | xargs grep --color --binary-files=without-match --line-number $what;
+    sed -n $i,$(( i + topBoundary ))p $TEMP_FILE | sed 's/ /\\ /g' | sed "s/'//g" | xargs grep --color --binary-files=without-match --line-number $what;
     numlines=$(( numlines - topBoundary ));
     if (( numlines <= 0 )); then
         break;
     fi;
 done;
 
+DEBUG "Done grepping the files"
+
+
 # clean up the temp files if they exist
 if [ -f "$TEMP_FILE" ]; then
+    DEBUG "Deleting TEMP_FILE $TEMP_FILE"
     rm -f "$TEMP_FILE";
 fi
-if [ -f "$TEMP_FILE_GIT" ]; then
-    rm -f "$TEMP_FILE_GIT";
-fi
 
+DEBUG "All done!"
 
